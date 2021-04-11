e39c604af3d695008572d2ab66ff169d06f53ebe
Siddhesh Kankekar <kankekas@tcd.ie>
2021-04-11 11:38:48 +0100

    fix for vercel

---
+++ keys.js
@@ -0,0 +1,8 @@
+// API_KEY - https://developers.google.com/custom-search/v1/using_rest
+export const REACT_APP_API_KEY = "AIzaSyDOqtmrPsd8Fx6qm5iH7uqdRmO3jMomvDw";
+
+// CONTEXT_KEY - https://cse.google.com/cse/create/new
+export const REACT_APP_CONTEXT_KEY = "abe2286436f07a202";
+
+//<script async src="https://cse.google.com/cse.js?cx=abe2286436f07a202"></script>
+// <div class="gcse-search"></div>
